884fb228a0c9871c35f1701e68c7d76fa6fee2af
Noah Herron <jayssbronze@gmail.com>
2015-09-10 11:12:50 -0400

    still trying to get flower to show

--- src/js/common/SpriteTest.js
+++ src/js/common/SpriteTest.js
@@ -2,8 +2,8 @@
 	this is a test file for Sprtie
 */
 
-var filepath = ["../../res/player/test/sprite_test", ".png"];
-var sprite = new Sprite(filepath,5);
+//var filepath = ["../../res/player/test/sprite_test", ".png"];
+//var sprite = new Sprite(filepath,5);
 var img;
 
 // function preload(){
