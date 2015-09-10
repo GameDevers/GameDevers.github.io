1f38dee8cdfcdd5da432d9a7aa7a66bab65c0583
Noah Herron <jayssbronze@gmail.com>
2015-09-10 11:15:22 -0400

    got flower to work now lets try player

--- src/js/common/SpriteTest.js
+++ src/js/common/SpriteTest.js
@@ -2,8 +2,8 @@
 	this is a test file for Sprtie
 */
 
-//var filepath = ["../../res/player/test/sprite_test", ".png"];
-//var sprite = new Sprite(filepath,5);
+var filepath = ["/res/player/test/sprite_test", ".png"];
+var sprite = new Sprite(filepath,5);
 var img;
 
 // function preload(){
@@ -12,12 +12,12 @@ var img;
 
 function setup(){
 	createCanvas(720, 480);
-	img = loadImage("flower.jpg");
+	//img = loadImage("flower.jpg");
 }
 
 function draw(){
-	image(img,0,0);
+	//image(img,0,0);
 
-	// image(sprite.imageArray[sprite.imageIndex],720/2,480/2);
+	image(sprite.imageArray[sprite.imageIndex],720/2,480/2);
 	// sprite.nextImage();
 }
